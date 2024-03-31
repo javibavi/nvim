6dcf5a407df9b3d6552cbb631160b4438fd55836
javi_bavi <xavierwood3737@gmail.com>
2024-03-31 15:58:21 -0500

    Added a telescope venv selector and discord rich presence

---
+++ lua/plugins/venv-selector.lua
@@ -0,0 +1,13 @@
+return {
+  'linux-cultist/venv-selector.nvim',
+  opts = {
+    -- Your options go here
+    -- name = "venv",
+    -- auto_refresh = false
+  },
+  event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  keys = {
+    -- Keymap to open VenvSelector to pick a venv.
+    { '<leader>fv', '<cmd>VenvSelect<cr>' },
+  },
+}
